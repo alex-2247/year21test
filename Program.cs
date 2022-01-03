76965aa4ffab1f1b038967e54440bc9b8a468dde
alex-2247 <alex2247@mail.ru>
2022-01-03 19:39:28 +0500

    add method for honesting of array

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿
-// Подзадача 1. Задать массив А
-// Длину массива возьмем 25 элементов. Это достаточно много чтоб поиграться,
-// но не слишком, чтоб заипаться.
+﻿// метод формирования исходного массива. Длину назначим 25 элементов. 
+// Это достаточно много чтоб поиграться, но не слишком, чтоб заипаться.
 void FillSourceArray(int[] array, int minValue, int maxValue)
 {
     for (int index = 0; index < array.Length; index++)
@@ -10,6 +8,7 @@ void FillSourceArray(int[] array, int minValue, int maxValue)
         //Console.Write($"i={index}:{array[index]}; ");
     }
 }
+
 // метод печати массива
 void PrintIntArrToConsole(int[] array)
 {
@@ -18,4 +17,20 @@ void PrintIntArrToConsole(int[] array)
     {
         Console.Write($"{array[index]}; ");
     }
+}
+
+// метод формирования второго массива чётными элементами
+// Создать на его основе масcив B, отбрасывая те, которые нечётные
+int OddHonest(int[] arrayA, int[] arrayB)
+{
+    int indexB = 0;
+    for (int indexA = 0; indexA < arrayA.Length; indexA++)
+    {
+        if ((arrayA[indexA] % 2) == 0)    //текущий нечётный
+        {
+            arrayB[indexB] = arrayA[indexA];
+            indexB++;
+        }
+    }
+    return indexB;
 }
